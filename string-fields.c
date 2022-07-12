e20d3f162981cd4c56dcff248d111628f94336e8
Edison Chimezie <edison.chimezie01@gmail.com>
2022-07-12 16:32:39 -0700

    gets prrecision from the format string

--- string-fields.c
+++ string-fields.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
-/* Code Credit : Ugobo Thankgod and Paul Sobande */
+/* Code Credit : Eddie Chimezie and Munachi Brian Umehm*/
 /**
  * get_precision - gets the precision from the format string
  * @p: the format string
  * @params: the parameters struct
- * @ap: the argument pointer
+ * @aa: the argument pointer
  *
  * Return: new pointer
  */
-char *get_precision(char *p, params_t *params, va_list ap)
+char *get_precision(char *p, params_t *params, va_list aa)
 {
-    int d = 0;
+	int d = 0;
 
-    if (*p != '.')
-        return (p);
-    p++;
-    if (*p == '*')
-    {
-        d = va_arg(ap, int);
-        p++;
-    }
-    else
-    {
-        while (_isdigit(*p))
-            d = d * 10 + (*p++ - '0');
-    }
-    params->precision = d;
-    return (p);
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		d = va_arg(aa, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			d = d * 10 + (*p++ - '0');
+	}
+	params->precision = d;
+	return (p);
 }
