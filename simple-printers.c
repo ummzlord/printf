65a2700aade37b61aa2560fe8b3896c4e434798f
Edison Chimezie <edison.chimezie01@gmail.com>
2022-07-12 16:55:15 -0700

    simple printing

--- simple-printers.c
+++ simple-printers.c
@@ -10,69 +10,70 @@
  */
 int print_from_to(char *start, char *stop, char *except)
 {
-    int sum = 0;
+	int add = 0;
 
-    while (start <= stop)
-    {
-        if (start != except)
-            sum += _putchar(*start);
-        start++;
-    }
-    return (sum);
+	while (start <= stop)
+	{
+		if (start != except)
+			add += _putchar(*start);
+		start++;
+	}
+	return (add);
 }
 
 /**
  * print_rev - prints string in reverse
- * @ap: string
+ * @aa: string
  * @params: the parameters struct
  *
  * Return: number bytes printed
  */
-int print_rev(va_list ap, params_t *params)
+int print_rev(va_list aa, params_t *params)
 {
-    int len, sum = 0;
-    char *str = va_arg(ap, char *);
-    (void)params;
+	int len, add = 0;
+	char *str = va_arg(aa, char *);
+	(void)params;
 
-    if (str)
-    {
-        for (len = 0; *str; str++)
-            len++;
-        str--;
-        for (; len > 0; len--, str--)
-            sum += _putchar(*str);
-    }
-    return (sum);
+	if (str)
+	{
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			add += _putchar(*str);
+	}
+	return (add);
 }
 
 /**
  * print_rot13 - prints string in rot13
- * @ap: string
+ * @a: string
  * @params: the parameters struct
  *
  * Return: number bytes printed
  */
-int print_rot13(va_list ap, params_t *params)
+int print_rot13(va_list aa, params_t *params)
 {
-    int i, index;
-    int count = 0;
-    char arr[] =
-        "NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
-    char *a = va_arg(ap, char *);
-    (void)params;
+	int i, index;
+	int count = 0;
+	char arr[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(aa, char *);
+	(void)params;
 
-    i = 0;
-    index = 0;
-    while (a[i])
-    {
-        if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
-        {
-            index = a[i] - 65;
-            count += _putchar(arr[index]);
-        }
-        else
-            count += _putchar(a[i]);
-        i++;
-    }
-    return (count);
+	i = 0;
+	index = 0;
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z')
+		    || (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] - 65;
+			count += _putchar(arr[index]);
+		}
+		else
+			count += _putchar(a[i]);
+		i++;
+	}
+	return (count);
 }
