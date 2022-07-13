74c0fda81eebb5c3c535067640f4bbabedc4725f
Edison Chimezie <edison.chimezie01@gmail.com>
2022-07-12 17:43:10 -0700

    Function converter

--- number.c
+++ number.c
@@ -40,21 +40,21 @@ char *convert(long int num, int base, int flags, params_t *params)
 
 /**
  * print_unsigned - prints unsigned integer numbers
- * @aa: argument pointer
+ * @ap: argument pointer
  * @params: the parameters struct
  *
  * Return: bytes printed
  */
-int print_unsigned(va_list aa, params_t *params)
+int print_unsigned(va_list ap, params_t *params)
 {
 	unsigned long l;
 
 	if (params->l_modifier)
-		l = (unsigned long)va_arg(aa, unsigned long);
+		l = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(aa, unsigned int);
+		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
-		l = (unsigned int)va_arg(aa, unsigned int);
+		l = (unsigned int)va_arg(ap, unsigned int);
 	params->unsign = 1;
 	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
 }
@@ -63,14 +63,14 @@ int print_unsigned(va_list aa, params_t *params)
 
 /**
  * print_address - prints address
- * @aa: argument pointer
+ * @ap: argument pointer
  * @params: the parameters struct
  *
  * Return: bytes printed
  */
-int print_address(va_list aa, params_t *params)
+int print_address(va_list ap, params_t *params)
 {
-	unsigned long int n = va_arg(aa, unsigned long int);
+	unsigned long int n = va_arg(ap, unsigned long int);
 	char *str;
 
 	if (!n)
